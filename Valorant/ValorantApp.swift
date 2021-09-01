f27faf23f1921a3e92cec5f65bf3c2b0475045cd
danya <daniil.orlov16@yandex.ru>
2021-09-01 15:46:24 +0300

    Initial Commit

---
+++ Valorant/ValorantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantApp.swift
+//  Valorant
+//
+//  Created by danyaq on 01.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ValorantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
